c64fcc117c0d2bb6b9c057b6aaad5fbcdc80e2f9
Jun Deng <jun@hai.localdomain>
2016-06-05 22:05:34 +0200

    new branch for osx

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,7 +1,7 @@
 ;;; Character sets
 
-(set-default-font "Bitstream Vera Sans Mono-11")
-(add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-11"))
+;;(set-default-font "Bitstream Vera Sans Mono-11")
+;;(add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-11"))
 
 (defcustom sanityinc/force-default-font-for-symbols nil
   "When non-nil, force Emacs to use your default font for symbols."
